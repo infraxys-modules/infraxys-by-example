6d11be425ab096b6f867b7f57b5644491f40d647
jeroen-manders <jeroen.manders@external.mckinsey.com>
2019-11-24 15:47:17 +0100

    Uncomment the grant check

--- packets/variable-block-modification/files/run-grant-example.sh
+++ packets/variable-block-modification/files/run-grant-example.sh
@@ -4,7 +4,7 @@ set -euo pipefail;
 
 function test_grant() {
 	log_info "Checking if it's ok to run test_grant.";
-	#check_grant --grant_name "i-can-execute-it";
+	check_grant --grant_name "i-can-execute-it";
 	log_info "'check_grant' didn't exit, so modification is OK";
 }
 
