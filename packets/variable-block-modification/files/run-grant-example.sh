46e51bbf443fdcfdae67d75b7cef865f7dffb238
jeroen-manders <jeroen.manders@external.mckinsey.com>
2019-11-24 15:43:19 +0100

    Add grant example

---
+++ packets/variable-block-modification/files/run-grant-example.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail;
+
+function test_grant() {
+	log_info "Checking if it's ok to run test_grant.";
+	#check_grant --grant_name "i-can-execute-it";
+	log_info "'check_grant' didn't exit, so modification is OK";
+}
+
+test_grant;
