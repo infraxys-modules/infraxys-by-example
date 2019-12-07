a5c2e614371e525235a4810420dd072316b4ef2c
Developer <developer>
2019-12-07 15:23:41 +0000

    Minor fixes

--- packets/dynamic-list-attributes/files/get_ec2_regions.sh
+++ packets/dynamic-list-attributes/files/get_ec2_regions.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-curl -s https://raw.githubusercontent.com/infraxys-modules/infraxys-by-example/data/master/attribute-images/regions/ec2_regions.json | jq -r '.Regions[] | .RegionName';
-
+curl -s https://raw.githubusercontent.com/infraxys-modules/infraxys-by-example/master/attributes/dynamic-lists/resources/ec2_regions.json | jq -r '.Regions[] | .RegionName';
