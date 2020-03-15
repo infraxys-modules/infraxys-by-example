0c9a03d51cd1588f5893d0e7fe092547af3e0bbd
Jeroen Manders <jeroen@manders.be>
2020-03-15 11:22:17 +0000

    Remove unnecessary arguments

--- packets/Execute over ssh/files/run-example.sh
+++ packets/Execute over ssh/files/run-example.sh
@@ -1,24 +1,23 @@
 remote_instance_name="$atozInstance1.getAttribute("instance_name")";
 
 #[[
-function_run_me_on_target_function_dependencies="show_dialog show_message_dialog";
+function_run_me_on_target_function_dependencies="show_dialog show_message_dialog wait_for_feedback";
 function run_me_on_target() {
-	local function_name="run_me_on_target" display_message config_filename return_filename;
+	local function_name="run_me_on_target" display_message;
 	import_args "$@";
-	check_required_arguments "$function_name" display_message config_filename return_filename;
-	
+	check_required_arguments "$function_name" display_message;
 	log_info "Displaying popup if running interactive. Otherwise displaying the message in the console.";
 	if [ "$INTERACTIVE" == "true" ]; then
-		show_message_dialog --message "$display_message";
+		show_message_dialog --message "$display_message $hostname.";
 	else
-		echo -e "\n\t$display_message\n";
+		echo -e "\n\t$display_message $hostname.\n";
 	fi;
 	
-	log_info "Contents of received configuration file '$config_filename':";
-	cat "$config_filename";
+	log_info "Contents of received configuration file '$transfer_file_remote_path':";
+	cat "$transfer_file_remote_path";
 	
-	log_info "Creating file '$return_filename'.";
-	cat > "$return_filename" << EOF
+	log_info "Creating file '$retrieve_file'.";
+	cat > "$retrieve_file" << EOF
 This is written
 on host $hostname
 by $(id -un)
@@ -27,7 +26,7 @@ IP config:
 
 EOF
 
-	ip addr show >> "$return_filename";
+	ip addr show >> "$retrieve_file";
 }
 
 log_info "Executing function 'run_me_on_target' on the example EC2-instance.";
@@ -41,9 +40,7 @@ execute_function_over_ssh \
 		--transfer_file_remote_path "/tmp/my-example.conf" \
         --retrieve_file "/tmp/remote_filename" \
         --retrieve_file_local_path "/tmp/retrieved_file.txt" \
-        --display_message "Hello from Infraxys" \
-        --config_filename "/tmp/my-example.conf" \
-        --return_filename "/tmp/remote_filename";
+        --display_message "Hello from ";
         
 log_info "Back from remote execution. Retrieved file contains:";
 cat "/tmp/retrieved_file.txt";
